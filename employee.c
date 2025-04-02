7cda99d0268ca4298f8ad45a55204062b9ec5945
Alwin8969 <alwinjoshy2028@cs.sjcetpalai.ac.in>
2025-04-02 16:10:43 +0530

    Add files via upload

---
+++ employee.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+struct Employee {
+  char name[50];
+  int id;
+  float salary;
+};
+int main() {
+  int n;
+  printf("Enter the number of employees: ");
+  scanf("%d", &n);
+
+  struct Employee employees[n];
+
+  for (int i = 0; i < n; i++) {
+    printf("\nEnter details for employee %d:\n", i + 1);
+    printf("Name: ");
+    scanf("%s", employees[i].name);
+    printf("Id: ");
+    scanf("%d", &employees[i].id);
+    printf("Salary: ");
+    scanf("%f", &employees[i].salary);
+}
+
+  printf("\nEmployee Details:\n");
+  for (int i = 0; i < n; i++) {
+    printf("\nEmployee %d:\n", i + 1);
+    printf("Name: %s\n", employees[i].name);
+    printf("ID: %d\n", employees[i].id);
+    printf("Salary: %.2f\n", employees[i].salary);
+  }
+
+  return 0;
+}
